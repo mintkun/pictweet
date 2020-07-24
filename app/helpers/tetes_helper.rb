e80b29871b43243d43e7a3d68d225964abcc0beb
tech-camp <tech-camp@A-108.local>
2020-07-24 11:37:32 +0900

    first commit

---
+++ app/helpers/tetes_helper.rb
@@ -0,0 +1,2 @@
+module TetesHelper
+end
---
+++ test/controllers/tetes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TetesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
